c847e5e90d986b2392e12845e98ba74127c86c76
HILAL HIBRIZI <128227144+HilalHibrizi@users.noreply.github.com>
2024-09-29 22:06:17 +0800

    Update app.py

--- dashboard/app.py
+++ dashboard/app.py
@@ -2,9 +2,6 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 import streamlit as st
-import os
-st.write("Current directory:", os.getcwd())
-# Membaca data dari CSV
 merged_df = pd.read_csv("dashboard/all.csv")
 
 
